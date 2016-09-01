900b81b3652510043b662622842bfa9b274f4cfb
Kiara Eldred <kiaraeldred1@gmail.com>
2016-09-01 13:16:15 -0400

    First lunch problem

---
+++ day4-lunch/lunchBoxPlot.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df_ctab = pd.read_csv( sys.argv[1], sep = "\t" )
+df2_ctab = pd.read_csv( sys.argv[2], sep = "\t" )
+
+df_roi = df_ctab[ "gene_name" ] == "Sxl"
+df_sxl = df_ctab[ df_roi ]
+df_roi2 = df_sxl[ "FPKM" ] > 0
+df_sxl_Fsort = df_sxl[ df_roi2 ]
+
+#print df_sxl_Fsort
+
+
+
+df2_roi = df2_ctab[ "gene_name" ] == "Sxl"
+df2_sxl = df2_ctab[ df_roi ]
+df2_roi2 = df2_sxl[ "FPKM" ] > 0
+df2_sxl_Fsort = df2_sxl[ df_roi2 ]
+
+#print df2_sxl_Fsort
+
+#============================================================#
+# CREATE SOME PLOTS
+
+# Extract the first feature from the dataset
+
+names = [ "SRR072893", "SRR07295" ]
+
+plt.figure()                       # Open a blank canvas
+plt.title("Sxl Transcripts in Samples") # Add a title to the top
+plt.boxplot([df_sxl_Fsort[ "FPKM" ], df2_sxl_Fsort[ "FPKM" ] ], labels=names )
+    # ...of the values for each SAMPLE
+	#labels=samples,                # ...with the species names as labels
+	#)
+plt.yscale( 'log' )
+plt.xlabel("Samples")              # Label the x-axis
+plt.ylabel( "log10(FPKM) of Sxl")
+plt.savefig("day4-lunch-1.png")   # Save the plot
+plt.close()                        # Close the canvas
+
+
